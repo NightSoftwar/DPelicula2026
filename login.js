b5a4033a466f5f100539c007904c0871ec48304a
NightSoftwar <103617522+NightSoftwar@users.noreply.github.com>
2025-11-10 02:07:23 +0000

    prueba

---
+++ login.js
@@ -0,0 +1,9 @@
+const usuario = document.getElementById("usuario");
+const passWord = document.getElementById("clave");
+const formularioInicioSeccion = document.getElementById("formulario");
+
+formularioInicioSeccion.addEventListener("submit",function (e){
+    e.preventDefault();
+
+    
+})
